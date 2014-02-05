2205bdf3f608f417c9a9fc2bee197f3c1a6347a4
= <michel.blaauw@gmail.com>
2014-02-06 00:05:37 +0100

    under construction

--- scrapy_bl/spiders/bl_spider.py
+++ scrapy_bl/spiders/bl_spider.py
@@ -8,18 +8,32 @@ from scrapy_bl.items import ScrapyBlItem
 class BlSpider(BaseSpider):
     name = "bl"
     allowed_domains = ["boekenliefde.nl"]
-
-    start_urls = ["http://boekenliefde.nl/editie-1243809-.html"]
+    start_urls = []
+    #2543810
+    for i in range(9999, 2543810):
+        new_url = "http://boekenliefde.nl/editie-" + str(i) + "-.html"
+        start_urls.append(new_url)
 
 
     def parse(self, response):
+        items = []
+
         hxs = HtmlXPathSelector(response)
 
-        isbn = hxs.select('//span[@itemprop="isbn"]/text()').extract()
-        ratingValue = hxs.select('//span[@itemprop="ratingValue"]/text()').extract()
-        bestRating = hxs.select('//span[@itemprop="bestRating"]/text()').extract()
-        ratingCount = hxs.select('//span[@itemprop="ratingCount"]/text()').extract()
-        bookEdition = hxs.select('//span[@itemprop="bookEdition"]/text()').extract()
-        numberOfPages = hxs.select('//span[@itemprop="numberOfPages"]/text()').extract()
+        item = ScrapyBlItem()
+        item['isbn'] = hxs.select('//span[@itemprop="isbn"]/text()').extract()
+        item['ratingValue'] = hxs.select('//span[@itemprop="ratingValue"]/text()').extract()
+        item['bestRating'] = hxs.select('//span[@itemprop="bestRating"]/text()').extract()
+        item['ratingCount'] = hxs.select('//span[@itemprop="ratingCount"]/text()').extract()
+        item['bookEdition'] = hxs.select('//span[@itemprop="bookEdition"]/text()').extract()
+        item['numberOfPages'] = hxs.select('//span[@itemprop="numberOfPages"]/text()').extract()
 
-        print isbn, numberOfPages, ratingValue, bookEdition
+        #item['Translator'] = hxs.select('//*[@id="bookinfo"]/div[4]/div[2]/a/text()').extract()
+        #item['Details'] = hxs.select('//*[@id="bookinfo"]/div[3]/div[2]/text()').extract()
+        #item['EditionDetails'] = hxs.select('//*[@id="bookinfo"]/div[2]/div[2]/text()').extract()
+        #item['Illustrations'] = hxs.select('//*[@id="bookinfo"]/div[3]/div[2]/a/text()').extract()
+
+        items.append(item)
+
+        return items
+
